06033a3dfa852fb28ab20105b41b7de8234e8414
Tan <nguyennt@spgroup.com.sg>
2018-07-31 16:58:39 +0800

    add Auth0 callback handler

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get "/auth/oauth2/callback" => "auth0/callback"
+  get "/auth/oauth2/failure" => "auth0/failure"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

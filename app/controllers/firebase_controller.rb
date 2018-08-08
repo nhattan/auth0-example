79bac7fe11efc14840e4df77a12c6daf53d1c557
Tan <nguyennt@spgroup.com.sg>
2018-08-08 14:16:21 +0800

    add omniauth and firebase stategy

--- app/controllers/firebase_controller.rb
+++ app/controllers/firebase_controller.rb
@@ -1,19 +1,19 @@
 class FirebaseController < ApplicationController
+  # GET /auth/:provider/callback?id_token=xxx
   def callback
     byebug
     session[:auth] = auth_hash
     redirect_to '/'
   end
 
-  # def signout
-  #   session[:auth] = nil
-  #   redirect_to '/'
-  # end
+  def signout
+    session[:auth] = nil
+    redirect_to '/'
+  end
 
   private
 
   def auth_hash
-    # request.env['omniauth.auth']
-    params['id_token']
+    request.env['omniauth.auth']
   end
 end
